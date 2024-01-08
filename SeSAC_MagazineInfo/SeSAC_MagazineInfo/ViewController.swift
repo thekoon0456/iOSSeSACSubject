b1b746fa877581bee8dcd3aa608a8be85f9a71ce
Deokhun KIM <thekoon0456@gmail.com>
2024-01-08 17:01:46 +0900

    [Feat] MagazineInfo 프로젝트 구성

---
+++ SeSAC_MagazineInfo/SeSAC_MagazineInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_MagazineInfo
+//
+//  Created by Deokhun KIM on 1/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
